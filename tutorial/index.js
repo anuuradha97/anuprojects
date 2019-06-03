e4b011abb4fe55b6796ef91b20b803258be91ead
anuuradha97 <50922553+anuuradha97@users.noreply.github.com>
2019-06-03 09:19:42 -0700

    Create index.js

---
+++ tutorial/index.js
@@ -0,0 +1,25 @@
+var express = require('express')
+var bodyParser = require('body-parser')
+var conController = require('./controllers/conController')
+
+var app = express()
+
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+app.set('view engine','ejs')
+app.use('/assets' , express.static('assets'));
+// fire controllers
+conController(app)
+
+app.get('/',function(req, res) {
+	res.render('index')
+})
+
+app.get('/contact', function(req, res) {
+	res.render('contact')
+})
+
+app.listen(8085,function() {
+	console.log('Started port 8085')
+})
